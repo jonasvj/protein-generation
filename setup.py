3b13f53f235f741d7bfab94522b6ffc6989d1821
jonasvj <jonas_vez@hotmail.com>
2020-11-07 03:04:06 +0100

    Initial skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Protein generation project for course 02456 Deep Learning at DTU',
+    author='Jonas Vestergaard Jensen',
+    license='MIT',
+)
