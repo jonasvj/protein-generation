d9381ad30efc4682af4a642879bab1769e380372
jonasvj <jonas_vez@hotmail.com>
2020-11-20 21:12:15 +0100

    Tidy

--- src/data/create_datasets.py
+++ src/data/create_datasets.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 import os
-import sys
 import random
 import subprocess
 import numpy as np
 import pandas as pd
 
-def random_split(entries, props, seed=42):
+def random_split(data, props, seed=42):
     """Randomly splits data frame into defined proportions.
     
     Args:
-        df: Data frame to split.
+        data: np.array of data to split
         props: List of proportions.
         seed: Seed for random split.
     
@@ -18,7 +17,7 @@ def random_split(entries, props, seed=42):
         List of data frames of specified proportions.
     """
     # Data splits
-    n = len(entries)
+    n = len(data)
     splits = np.cumsum([int(n*prop) for prop in props])
 
     # Lists of (random) indexes of specificied proportions 
@@ -27,9 +26,9 @@ def random_split(entries, props, seed=42):
     idxs = np.split(idxs, splits[:-1])
 
     # Split data frame 
-    entries_splitted = [entries[idx] for idx in idxs]
+    data_splitted = [data[idx] for idx in idxs]
 
-    return entries_splitted
+    return data_splitted
 
 if __name__ == '__main__':
     np.random.seed(42)
@@ -48,28 +47,18 @@ if __name__ == '__main__':
 
     # Filter data
     filtered_data = raw_data[raw_data['insulin'] == 'Yes']
-    not_insulin_data = raw_data[raw_data['insulin'] == 'No']
-    sampled_entries = np.random.choice(not_insulin_data.entry.unique(),
+    non_insulin_data = raw_data[raw_data['insulin'] == 'No']
+    sampled_entries = np.random.choice(non_insulin_data.entry.unique(),
                                        size=len(filtered_data.entry.unique()),
                                        replace=False)
 
-    not_insulin_data = not_insulin_data.loc[not_insulin_data['entry'].isin(
+    non_insulin_data = non_insulin_data.loc[non_insulin_data['entry'].isin(
         sampled_entries)]
     
-    filtered_data = filtered_data.append(not_insulin_data)
+    filtered_data = filtered_data.append(non_insulin_data)
 
-    """
-    organisms = list(set(list(not_insulin_data['organism_id'])))
-    n = 50
-
-    for Id in organisms:
-        df_organism = not_insulin_data[not_insulin_data['organism_id'] == Id]
-        if len(df_organism) >= n:
-            df_organism = df_organism.sample(n=n)
-        filtered_data = filtered_data.append(df_organism)
-    """
     # Keep only entry ID and sequence
-    filtered_data = filtered_data[['entry', 'bp', 'cc', 'mf',
+    filtered_data = filtered_data[['entry', 'organism', 'bp', 'cc', 'mf',
                                    'insulin', 'sequence']]
     
     # Split data into train, validation and test
