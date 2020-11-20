e3e7b4dadba402f75d4aeb22078f2e70ded92c09
jonasvj <jonas_vez@hotmail.com>
2020-11-20 15:09:15 +0100

    Change sampling

--- src/data/create_datasets.py
+++ src/data/create_datasets.py
@@ -6,7 +6,7 @@ import subprocess
 import numpy as np
 import pandas as pd
 
-def random_split(df, props, seed=42):
+def random_split(entries, props, seed=42):
     """Randomly splits data frame into defined proportions.
     
     Args:
@@ -18,7 +18,7 @@ def random_split(df, props, seed=42):
         List of data frames of specified proportions.
     """
     # Data splits
-    n = len(df)
+    n = len(entries)
     splits = np.cumsum([int(n*prop) for prop in props])
 
     # Lists of (random) indexes of specificied proportions 
@@ -27,35 +27,35 @@ def random_split(df, props, seed=42):
     idxs = np.split(idxs, splits[:-1])
 
     # Split data frame 
-    dfs = [df.iloc[idx,:] for idx in idxs]
-    print([len(df1) for df1 in dfs])
+    entries_splitted = [entries[idx] for idx in idxs]
 
-    return dfs
+    return entries_splitted
 
 if __name__ == '__main__':
+    np.random.seed(42)
 
     repo_dir = subprocess.run(
         ['git', 'rev-parse', '--show-toplevel'],
         stdout=subprocess.PIPE).stdout.decode().strip()
 
-    #Load data and rename columns
+    # Load data and rename columns
     raw_data = pd.read_csv(
-        os.path.join(repo_dir, 'data/interim/uniprot_table_tidy.txt'), sep='\t')
+        os.path.join(repo_dir, 'data/interim/uniprot_table_tidy.txt'),
+        sep='\t')
 
     raw_data.columns = ['entry', 'organism', 'bp', 'cc', 'mf',
                         'pfam', 'sequence', 'insulin']
 
-    #Filter data
+    # Filter data
     filtered_data = raw_data[raw_data['insulin'] == 'Yes']
     not_insulin_data = raw_data[raw_data['insulin'] == 'No']
     sampled_entries = np.random.choice(not_insulin_data.entry.unique(),
                                        size=len(filtered_data.entry.unique()),
                                        replace=False)
-    not_insulin_data = not_insulin_data.loc[not_insulin_data['entry'].isin(sampled_entries)]
-    sys.exit(1)
 
-    not_insulin_data = not_insulin_data.sample(n = len(filtered_data))
-
+    not_insulin_data = not_insulin_data.loc[not_insulin_data['entry'].isin(
+        sampled_entries)]
+    
     filtered_data = filtered_data.append(not_insulin_data)
 
     """
@@ -68,12 +68,17 @@ if __name__ == '__main__':
             df_organism = df_organism.sample(n=n)
         filtered_data = filtered_data.append(df_organism)
     """
-
     # Keep only entry ID and sequence
-    filtered_data = filtered_data[['entry', 'keywords', 'insulin', 'sequence']]
-  
+    filtered_data = filtered_data[['entry', 'bp', 'cc', 'mf',
+                                   'insulin', 'sequence']]
+    
     # Split data into train, validation and test
-    train_df, val_df, test_df = random_split(filtered_data, [0.8, 0.1, 0.1])
+    train_entries, val_entries, test_entries = random_split(
+        filtered_data.entry.unique(), [0.8, 0.1, 0.1])
+
+    train_df = filtered_data.loc[filtered_data['entry'].isin(train_entries)]
+    val_df = filtered_data.loc[filtered_data['entry'].isin(val_entries)]
+    test_df = filtered_data.loc[filtered_data['entry'].isin(test_entries)]
 
     # Save data sets
     train_df.to_csv(
