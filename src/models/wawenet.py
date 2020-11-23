ef289c275f81dcac6aa7838bc87fc034c1e3c1cc
jonasvj <jonas_vez@hotmail.com>
2020-11-23 12:59:10 +0100

    WaveNet architecture

---
+++ src/models/wawenet.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CausalConv1d(torch.nn.Conv1d):
+    """Causal 1d convolution"""
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+        self.__padding = (kernel_size - 1) * dilation
+
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=self.__padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+
+    def forward(self, input):
+        result = super(CausalConv1d, self).forward(input)
+        if self.__padding != 0:
+            return result[:, :, :-self.__padding]
+        return result
+
+class ResidualBlock(nn.Module):
+    """Residual block of WaveNet architecture"""
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1,
+    dilation=1):
+        super(ResidualBlock, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.dilation = dilation
+
+        self.filter_conv = CausalConv1d(in_channels=self.in_channels,
+                                        out_channels=self.in_channels,
+                                        kernel_size=self.kernel_size,
+                                        stride=self.stride,
+                                        dilation=self.dilation)
+        
+        self.gated_conv = CausalConv1d(in_channels=self.in_channels,
+                                        out_channels=self.in_channels,
+                                        kernel_size=self.kernel_size,
+                                        stride=self.stride,
+                                        dilation=self.dilation)
+        
+        self._1x1_conv = nn.Conv1d(in_channels=self.in_channels,
+                                  out_channels=self.out_channels,
+                                  kernel_size=1,
+                                  stride=1,
+                                  dilation=1)
+    
+    def forward(self, x):
+        # Filter convolution and gated convolution
+        x_f = torch.tanh(self.filter_conv(x))
+        x_g = torch.sigmoid(self.gated_conv(x))
+        
+        # Multiply filter convolution and gated convolution elementwise
+        skip = torch.mul(x_f, x_g)
+        skip = self._1x1_conv(skip)
+
+        residual = x + skip
+
+        return residual, skip
+
+class WaveNet(nn.Module):
+    """Neural network with the WaveNet architecture"""
+
+    def __init__(self, n_tokens, embedding_size, n_dilations, kernel_size=2, stride=1):
+        super(WaveNet, self).__init__()
+        self.n_tokens = n_tokens
+        self.embedding_size = embedding_size
+        self.n_dilations = n_dilations
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.residual_channels = 16
+        self.final_channels_1 = 8
+        self.final_channels_2 = 2
+        self.pad_idx = 0
+
+        self.encoder = nn.Embedding(self.n_tokens, self.embedding_size,
+                                    padding_idx=self.pad_idx)
+        
+        self.causal_conv = CausalConv1d(in_channels=self.embedding_size,
+                                        out_channels=self.residual_channels,
+                                        kernel_size=self.kernel_size,
+                                        stride=self.stride,
+                                        dilation=1)
+
+        self.residual_blocks = nn.ModuleList()     
+
+        for i in range(self.n_dilations):
+            dilation = 2**i
+            self.residual_blocks.append(
+                ResidualBlock(in_channels=self.residual_channels,
+                              out_channels=self.residual_channels,
+                              kernel_size=self.kernel_size,
+                              stride=self.stride,
+                              dilation=dilation))
+        
+        self.final_1x1_conv_1 = nn.Conv1d(in_channels=self.residual_channels,
+                                          out_channels=self.final_channels_1,
+                                          kernel_size=1,
+                                          stride=1,
+                                          dilation=1)
+        
+        self.final_1x1_conv_2 = nn.Conv1d(in_channels=self.final_channels_1,
+                                          out_channels=self.final_channels_2,
+                                          kernel_size=1,
+                                          stride=1,
+                                          dilation=1)
+
+    def forward(self, x):
+        x = self.encoder(x).permute(0,2,1)
+        residual = self.causal_conv(x)
+
+        skips_total = torch.zeros(residual.shape)
+        for residual_block in self.residual_blocks:
+            residual, skip = residual_block(residual)
+            skips_total += skip
+        
+        out = torch.relu(skips_total)
+        out = self.final_1x1_conv_1(out)
+        out = torch.relu(out)
+        out = self.final_1x1_conv_2(out)
+
+        return out
+
+if __name__ == '__main__':
+
+    input_ = torch.tensor([1, 2, 3, 3]).unsqueeze(0)
+    
+    net = WaveNet(4, 7, n_dilations=3, kernel_size=2, stride=1)
+    print(net)
+
+    output = net(input_)
+    print(output)
